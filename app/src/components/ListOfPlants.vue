a8de48612f3ff5d474b5153cd2ad8ff42761c0ff
arerorero <com.filippo@outlook.com>
2025-08-08 18:00:36 -0300

    clean list of plants file

--- app/src/components/ListOfPlants.vue
+++ app/src/components/ListOfPlants.vue
@@ -1,6 +1,5 @@
 <template>
   <div v-for="(plant, key, index) in crops" :key="index">
-    {{ plant.harvest_image }} {{ key }}
     <Avatar class="h-12 w-12">
       <AvatarImage :src="plant.harvest_image" :alt="key" />
       <AvatarFallback></AvatarFallback>
@@ -16,19 +15,8 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import plants from '@/data/plants.json'
 
-const harvestImages = import.meta.glob('@/assets/images/harvests/*.png', {
-  eager: true,
-  import: 'default',
-})
-
-const seedsImages = import.meta.glob('@/assets/images/seeds/*.png', {
-  eager: true,
-  import: 'default',
-})
-
 const crops = Object.fromEntries(
   Object.entries(plants).map(([key, value]) => {
-    // Monta a chave para acessar a imagem certa
     const harvests_path = `/src/assets/images/harvests/${key}.png`
     const seeds_path = `/src/assets/images/seeds/${key}_seeds.png`
 
@@ -42,8 +30,6 @@ const crops = Object.fromEntries(
     ]
   }),
 )
-
-console.log(crops)
 </script>
 
 <style scoped></style>
