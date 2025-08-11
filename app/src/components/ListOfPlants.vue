b21fd8737ba39e50c1887d5a0fbd8dfdacdaa35e
arerorero <com.filippo@outlook.com>
2025-08-11 01:14:02 -0300

    trying to organize the list of plants :()

--- app/src/components/ListOfPlants.vue
+++ app/src/components/ListOfPlants.vue
@@ -1,19 +1,39 @@
 <template>
-  <div v-for="(plant, key, index) in crops" :key="index">
-    <Avatar class="h-12 w-12">
-      <AvatarImage :src="plant.harvest_image" :alt="key" />
-      <AvatarFallback></AvatarFallback>
-    </Avatar>
-    <Avatar class="h-12 w-12">
-      <AvatarImage :src="plant.seeds_image" :alt="`${key}_seed`" />
-      <AvatarFallback></AvatarFallback>
-    </Avatar>
-  </div>
+  <section class="grid grid-cols-12 gap-4 justify-items-center">
+    <div v-for="(plant, key, index) in crops" :key="index" class="col-span-2 m-5">
+      <Card>
+        <CardHeader>
+          <CardTitle>{{ key }}</CardTitle>
+          <CardDescription class="flex items-center">
+            <img class="m-2" :src="plant.harvest_image" alt="harvest image" />
+            <img class="m-2" :src="plant.seeds_image" alt="seed image" />
+          </CardDescription>
+        </CardHeader>
+        <CardContent> price: {{ plant.price }}$ </CardContent>
+        <CardFooter>
+          <ul>
+            seasons:
+            <li v-for="(season, index) in plant.season" :key="index">
+              {{ season }}
+            </li>
+          </ul>
+        </CardFooter>
+      </Card>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import plants from '@/data/plants.json'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 const crops = Object.fromEntries(
   Object.entries(plants).map(([key, value]) => {
